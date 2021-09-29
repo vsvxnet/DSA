c1822a4a087def09194b4eb3586d6ae8d461b709
shawn.akhter@gmail.com <shawn.akhter@gmail.com>
2021-09-28 23:37:00 -0400

    Updates

--- DataStructuresAlgorithms/DataStructuresAlgorithms.cpp
+++ DataStructuresAlgorithms/DataStructuresAlgorithms.cpp
@@ -1,11 +1,61 @@
 // DataStructuresAlgorithms.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-#include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
 #include "MemoryCell.h"
 using namespace std;
+
+int StrLength(char input[])
+{
+	int cnt = 0;
+	for (int i = 0; input[i] != '\0'; i++)
+	{
+		cnt++;
+	}
+	return cnt;
+}
+
+void StrReverse(char input[])
+{
+	int s = 0;
+	int e = StrLength(input) - 1;
+
+	while (s < e)
+	{
+		swap(input[s], input[e]);
+		s++;
+		e--;
+	}
+}
+
+void PrintAllPrefixes(char str[])
+{
+	for (int e = 0; str[e] != '\0'; e++)
+	{
+		for (int s = 0; s <= e; s++)
+		{
+			cout << str[s];
+		}
+		cout << endl;
+	}
+}
+
+void PrintArray(int a[][5], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
 int main()
 {
+	
 	MemoryCell<int> int_cells(10), int_cells3(5);
 
 	MemoryCell<int> int_cells1(int_cells);
@@ -18,17 +68,64 @@ int main()
 
 	MemoryCell<int> myList(10);
 
+		cout << "Displaying before creating MemoryCell: " << endl;
+		myList.Display();
+
 	myList.Create();
-	//myList.Display();
-	myList.Append(&myList, 6);
-	//myList.Display();
-	myList.Append(&myList, 7);
-	myList.Display();
-	int deleteResult = myList.Delete(&myList, 7);
 
-	myList.Display();
-	cout << "Linear search \"4\" , index =" << myList.LinearSearch(4) << endl;
-	cout << "Binary search \"4\" , index =" << myList.BinarySearch(3) << endl;
+		cout << "Displaying after creating MemoryCell: " << endl;
+		myList.Display();
 
+		cout << "Linear search \"4\", index =" << myList.LinearSearch(4) << endl;
+		if (!myList.IsSorted())
+		{
+			cout << "List is not sorted for binary search. Sorting.." << endl;
+			myList.BubbleSort(&myList);
+			myList.Display();
+		}
+			cout << "Binary search \"4\", index =" << myList.BinarySearch(4) << endl;
+			cout << "Recursive binary search \"4\", index =" << myList.RecursiveBinarySearch(0, myList.GetLength(), 4) << endl;
+
+		
+		
+		
+	//myList.Append(&myList, 6);
+	//myList.Display();
+	//myList.Append(&myList, 7);
+	//myList.Display();
+	//int deleteResult = myList.Delete(&myList, 7);
+
+//	cout << "Linear search \"4\" , index =" << myList.LinearSearch(4) << endl;
+//	cout << "Binary search \"4\" , index =" << myList.BinarySearch(3) << endl;
+	
+	//cout << "Before selection sort: " << endl;
+	//myList.Display();
+	//myList.SelectionSort();
+	//myList.Display();
+
+	//cout << "Before bubble sort: " << endl;
+	//myList.Display();
+	//myList.BubbleSort();
+	//myList.Display();
+	
+	//cout << "Before STL sort: " << endl;
+	//myList.Display();
+	//myList.STLSort();
+	//myList.Display();
+
+	//cout <<"Factorial: " << myList.Factorial(5) << endl;
+	//cout <<"Fibonacci: " << myList.Fibonacci(10) << endl;
+	//cout <<"Power: " << myList.Power(2,6) << endl;
+
+	//myList.RecursivePrint(10);
+	//cout << myList.DigitCount(534322121) << endl;
+	//cout << myList.IsSortedHelper(&myList) << endl;
+	/*
+	int a[] = { 5,6,6,8,9,10,11,11 };
+	int ans = 0;
+	myList.CountOccurance(a, 8, 11, 0, ans);
+
+	cout << "count of 11 is " << ans << endl;
+	*/
 	return 0;
 }
--- DataStructuresAlgorithms/MemoryCell.h
+++ DataStructuresAlgorithms/MemoryCell.h
@@ -2,16 +2,17 @@
 #ifndef MemoryCell_H
 #define MemoryCell_H
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 template<class T> class MemoryCell
 {
 private:
-	T *A;
+	T* A;
 	int size;
 	int length;
 public:
-	explicit MemoryCell(int initial_size) :size(initial_size) 
+	explicit MemoryCell(int initial_size) :size(initial_size)
 	{
 		cout << "Constructor: MemoryCell of size " << this->size << endl;
 		A = new T[this->size];
@@ -25,14 +26,14 @@ public:
 
 	MemoryCell(const MemoryCell& rhs)
 	{
-		A = new T{*rhs.A};
+		A = new T{ *rhs.A };
 		size = rhs.size;
 		length = rhs.length;
 		cout << "CopyConstructor(): MemoryCell of size " << this->size << endl;
 
 	}
 
-	MemoryCell(MemoryCell&& rhs) :A{ rhs.A }, size{ rhs.size }, length{rhs.length}
+	MemoryCell(MemoryCell&& rhs) :A{ rhs.A }, size{ rhs.size }, length{ rhs.length }
 	{
 		rhs.A = nullptr;
 		cout << "MoveConstructor(): MemoryCell of size " << this->size << endl;
@@ -51,7 +52,7 @@ public:
 		return *this;
 	}
 
-	MemoryCell& operator=(MemoryCell && rhs)
+	MemoryCell& operator=(MemoryCell&& rhs)
 	{
 		cout << "Move-Assignment(): " << endl;
 
@@ -61,7 +62,7 @@ public:
 		return *this;
 	}
 
-	int read() const
+	T read() const
 	{
 		return *A;
 	}
@@ -99,7 +100,12 @@ public:
 		}
 		cout << endl;
 	}
-	
+
+	int GetLength()
+	{
+		return length;
+	}
+
 	void Append(MemoryCell* m, int x)
 	{
 		if (m->length < m->size)
@@ -138,10 +144,10 @@ public:
 		}
 	}
 
-	int LinearSearch(T target_value) 
+	int LinearSearch(T target_value)
 	{
 		int i;
-		for (int i = 0; i <length; i++)
+		for (int i = 0; i < length; i++)
 		{
 			if (target_value == A[i])
 			{
@@ -150,54 +156,429 @@ public:
 		}
 		return -1;
 	}
+	
+	bool IsSorted()
+	{
+		if (length == 0 || length == 1)
+		{
+			return true;
+		}
 
-	int BinarySearch(T target_value)
+		if (A[0] > A[1])
+		{
+			return false;
+		}
+		return IsSorted(A + 1, length - 1);
+	}
+	
+	bool IsSorted(T a[], int length)
+	{
+
+		if (length == 0 || length == 1)
+		{
+			return true;
+		}
+
+		if (a[0] > a[1])
+		{
+			return false;
+		}
+
+		return IsSorted(a + 1, length - 1);
+	}
+	
+	void BubbleSort(MemoryCell *m)
+	{
+		for (int cnt = 1; cnt <= m->length - 1; cnt++)
+		{
+			int flag = 0;
+			for (int j = 0; j <= m->length - 2; j++)
+			{
+				if (m->A[j] > m->A[j + 1])
+				{
+					swap(m->A[j], m->A[j + 1]);
+					flag = 1;
+				}
+			}
+
+			if (flag == 0)
+			{
+				break;
+			}
+		}
+	}
+	
+	void SelectionSort(MemoryCell* m)
+	{
+		for (int i = 0; i <= m->length - 2; i++)
+		{
+			int smallest = i;
+			for (int j = i + 1; j <= m->length - 1; j++)
+			{
+				if (this->A[j] < this->A[smallest])
+				{
+					smallest = j;
+				}
+			}
+			swap(this->A[i], this->A[smallest]);
+		}
+	}
+
+	void SortSTL(MemoryCell* m)
+	{
+		sort(m->A, m->A + m->length);
+	}
+
+	int BinarySearch(T search_value)
 	{
 		int low, mid, high;
 		low = 0;
-		high = length - 1;
+		high = GetLength() - 1;
 
 		while (low <= high)
 		{
 			mid = (low + high) / 2;
-			if (target_value == A[mid])
+			if (search_value == A[mid])
 			{
 				return mid;
 			}
-			else if (target_value < A[mid])
+			else if (search_value < A[mid])
 			{
 				high = mid - 1;
 			}
 			else
 			{
-				low = mid1 + 1;
+				low = mid + 1;
 			}
 		}
 		return -1;
 	}
 
-	int RecursiveBinarySearch(int low, int high, T target_value)
+	int ComputeFactorial(int n)
 	{
-		int mid = 0;
-		if (low < high)
+		// base case, 0! = 1
+		if (n == 0)
 		{
-			mid = (low + high) / 2;
-			if (target_value == A[mid])
-			{
-				return mid;
-			}
-			else if (target_value < A[mid])
-			{
-				RecursiveBinarySearch(low, mid-1, target_value);
-			}
-			else
-			{
-				RecursiveBinarySearch(mid + 1, high, target_value);
-			}
+			return 1;
 		}
-		return -1;
+
+		int subsetAnswer = ComputeFactorial(n - 1);
+		int ans = n * subsetAnswer;
+		return ans;
 	}
 
+	/*
+	
+	void STLSort()
+	{
+		sort(A, A + length);
+	}
+
+	int Factorial(int n)
+	{
+		if (n == 0)
+		{
+			return 1;
+		}
+
+		int smallAns = Factorial(n - 1);
+		int ans = n * smallAns;
+		return ans;
+	}
+
+	int Fibonacci(int n)
+	{
+		if (n == 0)
+		{
+			return 0;
+		}
+
+		if (n == 1)
+		{
+			return 1;
+		}
+
+		int so1 = Fibonacci(n - 1);
+		int so2 = Fibonacci(n - 2);
+
+		return so1 + so2;
+	}
+
+	int Power(int x, int n)
+	{
+		if (n == 0)
+		{
+			return 1;
+		}
+
+		int smallOutput = Power(x, n - 1);
+		return x * smallOutput;
+	}
+
+	void RecursivePrint(int n)
+	{
+		if (n == 0)
+		{
+			return;
+		}
+
+		RecursivePrint(n - 1);
+
+		cout << n << endl;
+		return;
+	}
+
+	int DigitCount(int n)
+	{
+		if (n == 0)
+		{
+			return 0;
+		}
+
+		int smallAns = DigitCount(n / 10);
+
+		return smallAns + 1;
+	}
+
+	int DigitSum(int n)
+	{
+		if (n == 0)
+		{
+			return 0;
+		}
+
+		int smallAns = DigitSum(n / 10);
+
+		int last_digit = n % 10;
+		return smallAns + last_digit;
+	}
+
+	int MultiplyDigit(int m, int n)
+	{
+		if (n == 0)
+		{
+			return 0;
+		}
+
+		int smallAns = MultiplyDigit(m, n - 1);
+
+		return smallAns + m;
+	}
+
+	int CountZeros(int n)
+	{
+		if (n == 0)
+		{
+			return 0;
+		}
+
+		int smallAns = CountZeros(n / 10);
+
+		int last_digit = n & 10;
+
+		if (last_digit == 0)
+		{
+			return 1 + smallAns;
+		}
+		else
+		{
+			return smallAns;
+		}
+	}
+
+	double GeometricSum(int k)
+	{
+		if (k == 0)
+		{
+			return 1;
+		}
+
+		double smallAns = GeometricSum(k - 1);
+
+		return smallAns + 1.0 / Power(2, k);
+	}
+
+	bool IsSortedHelper(MemoryCell *m)
+	{
+		return IsSorted(m->A, m->GetLength());
+	}
+
+	bool IsSorted(T a[], int length)
+	{
+
+		if (length == 0 || length == 1)
+		{
+			return true;
+		}
+
+		if (a[0] > a[1])
+		{
+			return false;
+		}
+
+		return IsSorted(a + 1, length - 1);
+	}
+
+	bool IsSorted2(T a[], int length)
+	{
+		bool IsSmallerSorted = IsSorted(a + 1, length - 1);
+		
+		if (!IsSmallerSorted)
+		{
+			return false;
+		}
+
+		if (a[0] > a[1])
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+
+	bool IsSorted3(T a[], int n)
+	{
+		if (length == 0 || length == 1)
+		{
+			return true;
+		}
+		if (a[n - 2] > a[n - 1])
+		{
+			return false;
+		}
+		return IsSorted3(a, n - 1);
+	}
+
+	int SumOfArray(int a[], int n)
+	{
+		if (n == 0)
+		{
+			return 0;
+		}
+		return a[0] + SumOfArray(a + 1, n - 1);
+	}
+
+	int SumOfArray2(int a[], int n)
+	{
+		if (n == 0)
+		{
+			return 0;
+		}
+		return a[n - 1] + SumOfArray2(a, n - 1);
+	}
+
+	int SumOfArray3(int a[], int n, int i)
+	{
+		if (i == n)
+		{
+			return 0;
+		}
+		return a[i] + SumOfArray3(a, n, i + 1);
+	}
+
+	int FindFirstIndex(int a[], int n, int x, int i)
+	{
+		// Recursive FindFirstIndex of x
+		if (i == n)
+		{
+			return -1;
+		}
+		if (a[i] == x)
+		{
+			return i;
+		}
+
+		return FindFirstIndex(a, length, x, i + 1);
+	}
+
+	int FindLastIndex(int a[], int length, int x, int i)
+	{
+		// Recursive FindLastIndex of x
+		if (i == -1)
+		{
+			return -3;
+		}
+
+		if (a[i] == x)
+		{
+			return i;
+		}
+
+		return FindLastIndex(a, length, x, i - 1);
+	}
+
+	int FindLastIndex2(int a[], int n, int x)
+	{
+		if (n == 0)
+		{
+			return -3;
+		}
+		if (a[n - 1] == x)
+		{
+			return n - 1;
+		}
+
+		return FindLastIndex2(a, length - 1, x);
+	}
+
+	int FindLastIndex3(int a[], int n, int x, int i)
+	{
+		if (i == n)
+		{
+			return -1;
+		}
+
+		int indexInRem = FindLastIndex3(a, length, x, i + 1);
+		if (indexInRem == -1) 
+		{
+			if (a[i] == x)
+			{
+				return i;
+			}
+			else 
+			{
+				return -1;
+			}
+		}
+		else {
+			return indexInRem;
+		}
+	}
+
+	void PrintAllPosition(int a[], int n, int x, int i)
+	{
+		if (i == n)
+		{
+			return;
+		}
+
+		if (a[i] == x)
+		{
+			cout << i << " ";
+		}
+		PrintAllPosition(a, length, x, i + 1);
+	}
+
+	void CountOccurance(int a[], int n, int x, int i, int& ans)
+	{
+		if (i == n)
+		{
+			return;
+		}
+
+		if (a[i] == x)
+		{
+			ans++;
+		}
+
+		CountOccurance(a, length, x, i + 1, ans);
+	}
+
+
+	*/
 };
 
 
